649b73cf857b8aef62c01117883b4647c9d7385b
Tyler Silverman <silverman.tyler@gmail.com>
2020-12-28 13:25:25 -0500

    edited home handlebar, added login and signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,46 @@
-// const router = require("./api/projectRoutes");
-
 const router = require('express').Router();
-const { Project } = require('../models');
+const { User, Project } = require('../models');
 
+
+//GET all galleries from the homepage
 router.get('/', async (req, res) =>{
     try {
-        const projects = await Project.findAll ({
+        const dbProjectData = await Project.findAll ({
+            include: [{
+                model: User,
+                attributes: ["name", "email"],
+            },
+        ],
         });
-        const newProjects = projects.map((projects) => 
+        const newProjects = dbProjectData.map((projects) => 
             projects.get({plain: true})
-        )
+        );
         console.log(newProjects);
-        res.render("home", {newProjects});
+        res.render("home", {
+            newProjects,
+            loggedIn: req.session.loggedIn,
+        });
     } catch (error) {
         res.status(400).json(error)
     }
-})
+});
+
+//this is getting the login.handlebars layout
 router.get('/login', (req, res) =>{
 
-    console.log("did it work with login", req.body)
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    console.log("SAVED LOGIN INFORMATION", req.body)
     res.render('login')
 });
 
+//this is posting the information that was submitted in the login.handlebars layout
 router.post('/login', (req, res) =>{
 
-    console.log("did it work with login", req.body)
+    console.log("POST LOGIN INFORMATION", req.body)
     res.render('login')
 });
 
