0954823dc79e16fb6c6c8b00f3c47769b3bc2d62
Tyler Silverman <silverman.tyler@gmail.com>
2021-01-14 21:33:29 -0500

    make it better

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,3 @@
 DROP DATABASE IF EXISTS forumblog;
 CREATE DATABASE forumblog;
 USE forumblog;
-
-CREATE TABLE IF NOT EXISTS `user` (
-    `id` INTEGER NOT NULL auto_increment , 
-    `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL, 
-    PRIMARY KEY (`id`)
-    )
-
-      CREATE TABLE IF NOT EXISTS `post` (
-    `id` INTEGER NOT NULL auto_increment , 
-    `name` VARCHAR(255) NOT NULL,
-    `description` VARCHAR(255) NOT NULL, 
-    'date_created' date,
-    PRIMARY KEY (`id`)
-    )
-
-       CREATE TABLE IF NOT EXISTS `comment` (
-    `id` INTEGER NOT NULL auto_increment , 
-    `name` VARCHAR(255) NOT NULL,
-    `description` VARCHAR(255) NOT NULL, 
-    'date_created' date,
-    PRIMARY KEY (`id`)
-    )
